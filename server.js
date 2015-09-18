3ffd69dde2e10e426c01fbb377e15e304bc1b47c
Tian Xia <xia.tian@husky.neu.edu>
2015-09-17 23:37:53 -0400

    add hello.html under pulib folder

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
