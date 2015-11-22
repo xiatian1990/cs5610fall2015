1abe197eb6964e53329c0f82c2b69b9e66d915de
Tian Xia <xia.tian@husky.neu.edu>
2015-11-22 16:06:45 -0500

    retry config server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-var bodyParser = require ('body-parser');
+//var bodyParser = require ('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-require("./public/assignment/server/app.js")(app);
+//require("./public/assignment/server/app.js")(app);
 app.listen(port, ipaddress);
